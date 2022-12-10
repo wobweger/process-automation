63d4aa45b6550d120fb791daf02093df83d3a4ca
wro <walter.obweger@gmail.com>
2022-12-10 10:36:37 +0100

    clean up src comment methods

--- src/providers/PrcAutDataProvider.ts
+++ src/providers/PrcAutDataProvider.ts
@@ -4,6 +4,10 @@ import * as path from 'path';
 import { AnyRecord } from 'dns';
 import { lch } from 'd3';
 
+interface IfcPrcAutJson {
+    prcAutJson:{ [k: string]: any };
+}
+
 export class PrcAutDataProvider implements vscode.TreeDataProvider<PrcAutTreeItem> {
 
 	private _onDidChangeTreeData: vscode.EventEmitter<PrcAutTreeItem | undefined | void> = new vscode.EventEmitter<PrcAutTreeItem | undefined | void>();
@@ -19,23 +23,17 @@ export class PrcAutDataProvider implements vscode.TreeDataProvider<PrcAutTreeIte
 	getTreeItem(element: PrcAutTreeItem): vscode.TreeItem {
 		return element;
 	}
-
+    /**
+     * read process automation json PrcAut.pa.json
+     */
 	getChildren(element?: PrcAutTreeItem): Thenable<PrcAutTreeItem[]> {
-    //getChildren(element?: PrcAutTreeItem): PrcAutTreeItem[] {
             if (!this.workspaceRoot) {
 			vscode.window.showInformationMessage('No PrcAutTreeItem in empty workspace');
 			return Promise.resolve([]);
 		}
 
 		if (element) {
-            var lstChild:PrcAutTreeItem[]=[];
-            //for (o in element.getChildren()){
-            //element.children
-            element.getChildren().forEach(function(o){
-                    lstChild.push(o);
-            });
-            return Promise.resolve(lstChild);
-			//return Promise.resolve(this.getPrcAutInJson(path.join(this.workspaceRoot, element.label, 'PrcAut.pa.json')));
+            return Promise.resolve(this.getTreeItemByPrcAutJson(element.getPrcAutJson()));
 		} else {
 			const packageJsonPath = path.join(this.workspaceRoot, 'PrcAut.pa.json');
 			if (this.pathExists(packageJsonPath)) {
@@ -45,74 +43,59 @@ export class PrcAutDataProvider implements vscode.TreeDataProvider<PrcAutTreeIte
 				return Promise.resolve([]);
 			}
 		}
-
 	}
-
 	/**
-	 * Given the path to package.json, read all its dependencies and devDependencies.
+	 * Given the path to process automation json,
+     * read content and build tree items
 	 */
 	private getPrcAutInJson(jsonPath: string): PrcAutTreeItem[] {
 		const workspaceRoot = this.workspaceRoot;
 		if (this.pathExists(jsonPath) && workspaceRoot) {
 			const prcAutJson = JSON.parse(fs.readFileSync(jsonPath, 'utf-8'));
-            //const prcAutDat=prcAutJson.data;
-            //const prcAutMap=new Map(Object.entries(prcAutDat));
-            //var tree={};
-			const toPrcAut = (moduleName: string, type:string,child:{ [k: string]: any }) : PrcAutTreeItem => {
-				if (child) {
-					var prcAutPar=new PrcAutTreeItem(moduleName, type,"", vscode.TreeItemCollapsibleState.Collapsed);
-                    var lChild:PrcAutTreeItem[]=[];
-                    for (const [sKey,o] of Object.entries(child)){
-                        console.log(sKey);
-                        var sType=o._type;
-                        if (sType){
-                            console.log(child[sKey]);
-                            var oChild=toPrcAut(sKey,sType,o);
-                            lChild.push(oChild);
-                            //var prcAutChild=toPrcAut(sKey, sType,"", child.get(sKey));
-                        }
-                    }
-                    //const lstPrcAut = child
-				    //    ? Object.keys(child).map(prcAut => toPrcAut(prcAut, child[prcAut]['_type'],child[prcAut]))
-				    //    : [];
-                    prcAutPar.setChildren(lChild);
-                    return prcAutPar;
-				} else {
-					return new PrcAutTreeItem(moduleName, type,"", vscode.TreeItemCollapsibleState.None, {
-						command: 'extension.openPackageOnNpm',
-						title: '',
-						arguments: [moduleName]
-					});
-				}
-			};
-            var lChild:PrcAutTreeItem[]=[];
-            for (const [sKey,o] of Object.entries(prcAutJson)){
-                console.log(sKey);
-                //var o=prcAutJson.get(sKey);
-                var sType=o._type;
-                if (sType){
-                    //var prcAutPar=new PrcAutTreeItem(sKey, sType,"", vscode.TreeItemCollapsibleState.Collapsed);
-                    //var prcAutJson.get(sKey);
-                    var oChild=toPrcAut(sKey,sType,o);
-                    lChild.push(oChild);
-                }
-                console.log(prcAutJson[sKey]);
-                //var sType=o.get('_type');
-            }
-
-			//const lstPrcAut = prcAutJson
-			//	? Object.keys(prcAutJson).map(prcAut => toPrcAut(prcAut, prcAutJson[prcAut]['_type'],prcAutJson[prcAut]))
-			//	: [];
-			//const devDeps = packageJson.devDependencies
-			//	? Object.keys(packageJson.devDependencies).map(dep => toDep(dep, packageJson.devDependencies[dep]))
-			//	: [];
-			//return deps.concat(devDeps);
-            return lChild;
+            
+            return this.getTreeItemByPrcAutJson(prcAutJson);
 		} else {
 			return [];
 		}
 	}
+    /**
+     * evaluate json dictionary, looks for valid type set
+     * checks if element has a child or children to create tree item
+     * @param prcAutJson 
+     * @returns list of tree items
+     */
+    private getTreeItemByPrcAutJson(prcAutJson:{ [k: string]: any }):PrcAutTreeItem[]{
+        if (prcAutJson){
 
+            let lChild:PrcAutTreeItem[]=[];
+            for (const [sKey,o] of Object.entries(prcAutJson)){
+                console.log(sKey);
+                console.log(prcAutJson[sKey]);
+                let sType=o._type;
+                if (sType){
+                    let iHasChild:number=0;
+                    for (const [sKey,oC] of Object.entries(o)){
+                        if (!sKey.startsWith('_')){
+                            iHasChild++;
+                        }
+                    }
+                    if (iHasChild>0){
+                        lChild.push(new PrcAutTreeItem(sKey, sType,o, vscode.TreeItemCollapsibleState.Collapsed));
+                    }else{
+                        lChild.push(new PrcAutTreeItem(sKey, sType,o, vscode.TreeItemCollapsibleState.None));
+                    }
+                }
+            }
+            return lChild;
+        }else{
+            return [];
+        }
+    }
+    /**
+     * check if file exists
+     * @param p 
+     * @returns 
+     */
 	private pathExists(p: string): boolean {
 		try {
 			fs.accessSync(p);
@@ -125,25 +108,22 @@ export class PrcAutDataProvider implements vscode.TreeDataProvider<PrcAutTreeIte
 }
 
 export class PrcAutTreeItem extends vscode.TreeItem {
-    children?:PrcAutTreeItem[];
+    prcAutJson?:{ [k: string]: any };
 	constructor(
 		public readonly label: string,
 		private readonly type: string,
-		private readonly version: string,
+		private readonly prcAutJson:{ [k: string]: any },
 		public readonly collapsibleState: vscode.TreeItemCollapsibleState,
 		public readonly command?: vscode.Command
 	) {
 		super(label, collapsibleState);
-
+        this.prcAutJson=prcAutJson;
+        this.version='';
 		this.tooltip = `${this.label}-${this.version}`;
 		this.description = this.version;
-        this.children=undefined;
 	}
-    public setChildren(params:PrcAutTreeItem[]) {
-        this.children=params;
-    }
-    public getChildren():PrcAutTreeItem[] {
-        return this.children;
+    public getPrcAutJson():{ [k: string]: any } {
+        return this.prcAutJson;
     }
 	iconPath = {
 		light: path.join(__filename, '..', '..', 'resources', 'light', 'dependency.svg'),
